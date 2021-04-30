cad10ac89a2ddd623d500c461ead584d31e9cf65
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-05-01 01:12:31 +0900

    CREATE | Design AddFragment Layout

--- app/src/main/java/com/example/todoone/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/todoone/fragments/add/AddFragment.kt
@@ -17,6 +17,8 @@ class AddFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        return view
     }
 }
--- app/src/main/java/com/example/todoone/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoone/fragments/list/ListFragment.kt
@@ -17,6 +17,8 @@ class ListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        return view
     }
 }
